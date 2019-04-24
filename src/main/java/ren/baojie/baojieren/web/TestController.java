16a9500c41e18f0bc580745158af59061f952baf
baojieren <2013142083@qq.com>
2019-04-24 11:35:57 +0800

    code 优化

--- src/main/java/ren/baojie/baojieren/web/TestController.java
+++ src/main/java/ren/baojie/baojieren/web/TestController.java
@@ -2,7 +2,6 @@ package ren.baojie.baojieren.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ren.baojie.baojieren.model.UserPO;
 import ren.baojie.baojieren.service.UserPOService;
@@ -11,20 +10,19 @@ import javax.annotation.Resource;
 
 @Slf4j
 @RestController
-@RequestMapping("/baojie")
 public class TestController {
 
     @Resource
     private UserPOService userPOService;
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String test(String name) {
         UserPO userPO = new UserPO();
         userPO.setName(name);
         userPO.setAge(19);
         userPO.setPassword("123123");
         userPOService.insertSelective(userPO);
-        return "你输入的：" + name;
+        return "you enter ：" + name;
     }
 
 }
